15aec2bf24b9a16f888fd3bd352a66e75044cbb6
Dhrubot <dhrubo.dh@gmail.com>
2020-07-12 06:16:45 -0400

    Building out the nested forms for user in Order

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,9 +1,10 @@
 class OrdersController < ApplicationController
 
-    before_action :current_cart, :logged_in?, :redirect_if_not_logged_in, :current_user, :set_cart
+    before_action :current_cart, :logged_in?, :current_user, :set_cart
 
     def new
         @order = Order.new
+        @order.build_buyer
     end
 
     def create
@@ -12,20 +13,27 @@ class OrdersController < ApplicationController
         @order.buyer = current_user
 
         if @order.save
+            current_cart.cart_products.destroy_all
+
             redirect_to order_path(@order)
         else
             flash[:danger] = "Something Went wrong. Try logging in again."
+
+            redirect_to carts_path
         end
     end
 
     def show
+        @order = Order.find_by_id(params[:id])
     end
 
 
     private
 
     def order_params
-        params.require(:order).permit(:address_field_1, :address_field_2, :city, :state, :zipcode, :payment_method)
+        params.require(:order).permit(:address_field_1, :address_field_2,
+         :city, :state, :zipcode, :payment_method, buyer_attributes: [:firstname, 
+         :lastname, :username, :email, :password, :password_confirmation])
     end
 
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,7 @@
 class Order < ApplicationRecord
   belongs_to :buyer, :class_name => "User"
   belongs_to :cart
+  accepts_nested_attributes_for :buyer
+
+
 end
