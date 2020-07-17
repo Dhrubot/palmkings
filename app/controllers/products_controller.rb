46b2fb14ab7903dc2b1918400fbb290ef1a900da
Dhrubot <dhrubo.dh@gmail.com>
2020-07-17 03:25:16 -0400

    fixed access for products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,6 @@
 class ProductsController < ApplicationController
     
-    # before_action :logged_in?, :current_user, :redirect_if_not_logged_in, except: [:index, :show]
+    before_action :logged_in?, :current_user, :redirect_if_not_logged_in, except: [:index, :show]
 
     def index
         if params[:user_id] && user = User.find_by(id: params[:user_id])
