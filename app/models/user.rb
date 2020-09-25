1464100f68f8d6495d115a33eda7b9ca031b31f2
Dhrubot <dhrubo.dh@gmail.com>
2020-09-25 09:46:33 -0400

    Added a method that combines both omniauth login to one single method

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,20 +12,36 @@ class User < ApplicationRecord
 
     before_save :downcase_attributes
 
-    def self.from_google(auth)
-        where(email: auth.info.email).first_or_initialize do |user| # initalizes the user with the 'new' method. doesn't save it.
-            user.firstname = auth.info.first_name
-            user.lastname = auth.info.last_name
-            user.username = auth.info.name.split.join('.')
-            user.email = auth.info.email
-            user.password = SecureRandom.hex
-        end
-    end
+    # def self.from_google(auth)
+    #     where(email: auth.info.email).first_or_initialize do |user| # initalizes the user with the 'new' method. doesn't save it.
+    #         user.firstname = auth.info.first_name
+    #         user.lastname = auth.info.last_name
+    #         user.username = auth.info.name.split.join('.')
+    #         user.email = auth.info.email
+    #         user.password = SecureRandom.hex
+    #     end
+    # end
 
-    def self.from_facebook(auth)
+    # def self.from_facebook(auth)
+    #     where(email: auth.info.email).first_or_initialize do |user|
+    #         user.firstname = auth.info.name.split[0]
+    #         user.lastname = auth.info.name.split[1]
+    #         user.username = auth.info.name.split.join('.')
+    #         user.email = auth.info.email
+    #         user.password = SecureRandom.hex
+    #     end
+    # end
+
+    def self.new_from_omniauth(auth)
         where(email: auth.info.email).first_or_initialize do |user|
-            user.firstname = auth.info.name.split[0]
-            user.lastname = auth.info.name.split[1]
+
+            if auth.provider == "facebook"
+                user.firstname = auth.info.name.split[0]
+                user.lastname = auth.info.name.split[1]
+            else
+                user.firstname = auth.info.first_name
+                user.lastname = auth.info.last_name
+            end
             user.username = auth.info.name.split.join('.')
             user.email = auth.info.email
             user.password = SecureRandom.hex
